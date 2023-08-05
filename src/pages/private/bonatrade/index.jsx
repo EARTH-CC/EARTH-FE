0ff46dc9918e6813e0a143c797d3d7f1454b2be7
MatthewRomero007 <113237170+MatthewRomero007@users.noreply.github.com>
2023-08-06 03:58:01 +0800

    BillingStatement

---
+++ src/pages/private/bonatrade/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Box, Grid } from "@mui/material";
+import TopBar from "../../../components/PrivateComponents/ugtrade/TopBar/topBar";
+import BillingStatement from "./scenes/BillingStatement";
+import MyChart from "../../../components/PrivateComponents/ugtrade/Charts/StaticChart";
+import MyChart2 from "../../../components/PrivateComponents/ugtrade/Charts/StaticChart2";
+
+function Bonatrade() {
+  return (
+    <Box>
+      <TopBar />
+      <Grid container spacing={0} my={2}>
+        <Grid
+          item
+          xs={6}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <MyChart />
+          <MyChart2 />
+        </Grid>
+        <Grid item xs={6}>
+          <BillingStatement />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default Bonatrade;
