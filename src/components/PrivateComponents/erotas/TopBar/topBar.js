0c9262be2245ff26c0441c7c9e04a3a72a6d6e26
Mark Joseph Salem <125126248+MarkSalem804@users.noreply.github.com>
2023-08-06 14:54:55 +0800

    added erotas

--- src/components/PrivateComponents/erotas/TopBar/topBar.js
+++ src/components/PrivateComponents/erotas/TopBar/topBar.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
+// import { Box } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -26,29 +27,56 @@ function StickyAppBar() {
         }}
       >
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            onClick={handleGoBack}
-            sx={{ mr: 2, ml: 5 }}
-          >
-            <img src={Logo} alt="Logo" style={{ height: "50px" }} />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
+          <Box
             sx={{
-              flexGrow: 1,
-              color: "#830076", // Your desired color for the text
-              fontFamily: "Poppins", // Your desired font family
-              fontSize: 18, // Your desired font size
-              fontWeight: "bold", // Your desired font weight
+              // height: "60vh",
+              mt: "10px",
+              margin: "auto",
+              textAlign: "center",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              ml: "27%",
             }}
           >
-            Erotas Portus Incorporated
-          </Typography>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleGoBack}
+              sx={{ mr: 2, ml: 5 }}
+            >
+              <img src={Logo} alt="Logo" style={{ height: "100px" }} />
+            </IconButton>
+            <Box>
+              <Typography
+                variant="h1"
+                sx={{ fontWeight: "bold", color: "#231a8b" }}
+              >
+                MANILA WATERFRONT CITY <break />
+              </Typography>
+              <Typography
+                variant="h2"
+                sx={{
+                  fontWeight: "bold",
+                  color: "#606060",
+                  my: "-5px",
+                  letterSpacing: "4px",
+                }}
+              >
+                RECLAMATION PROJECT
+              </Typography>
+              <Box
+                sx={{
+                  backgroundColor: "black",
+                  height: "6px",
+                  width: "500px",
+                  my: "9px",
+                }}
+              />
+            </Box>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
