28c89275f13d914889d61a28cd3acb087247fef3
Ray <107069990+hernandezraymondm@users.noreply.github.com>
2023-09-04 12:39:34 +0800

    static creds if BE is OFFLINE

--- src/services/account-service.js
+++ src/services/account-service.js
@@ -4,14 +4,58 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:9000";
 
-function authenticate(account) {
+function isBackendAvailable() {
   return axios
-    .post(`${BASE_URL}/login`, account, {
-      headers: { "Content-Type": "application/json" },
-      withCredentials: true,
-    })
-    .then((res) => res.data);
+    .get(`${BASE_URL}/ping`)
+    .then(() => true) // If the request succeeds, the backend is available
+    .catch(() => false); // If the request fails, the backend is not available
 }
+
+function authenticate(account) {
+  return isBackendAvailable().then((backendAvailable) => {
+    if (backendAvailable) {
+      // If the backend is available
+      return axios
+        .post(`${BASE_URL}/login`, account, {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        })
+        .then((res) => res.data)
+        .catch((error) => {
+          console.error("Backend request error:", error);
+          throw error;
+        });
+    }
+    // If the backend is not available, simulate a successful login using static credentials
+    const staticAccount = {
+      valid: true,
+      message: "Login successful",
+      data: {
+        uuid: 1,
+        username: "eg",
+        password:
+          "$2b$10$6Hq/7njiVUixrRytZmj.XuPsGqxvet.dAVhdYyIQsLINk/GuZBgee",
+        firstname: "Mark",
+        lastname: "Salem",
+        role: "superadmin",
+        refresh_token: "YOUR_STATIC_REFRESH_TOKEN",
+        status: 1,
+        accessToken: "YOUR_STATIC_ACCESS_TOKEN",
+      },
+    };
+
+    return Promise.resolve(staticAccount); // Resolve the promise with static data
+  });
+}
+
+// function authenticate(account) {
+//   return axios
+//     .post(`${BASE_URL}/login`, account, {
+//       headers: { "Content-Type": "application/json" },
+//       withCredentials: true,
+//     })
+//     .then((res) => res.data);
+// }
 
 function logout(account) {
   return axios
