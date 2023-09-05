981d9f982b863c0fe88bb28784aa0b65b6f57650
itschrollo <81880211+itschrollo@users.noreply.github.com>
2023-09-05 16:10:59 +0800

    purchase order and register

--- src/pages/private/eglogistics/scenes/procurement/pr_order/index.jsx
+++ src/pages/private/eglogistics/scenes/procurement/pr_order/index.jsx
@@ -1,46 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box, Button, Divider, Typography, useTheme } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
-import PurchaseRequestModal from "modal/Procurement/PurchaseRequestModal";
 import Header from "../../../../../../components/PrivateComponents/eglogistics/Header";
 import themes from "../../../../../../themes/theme";
 import PurchaseOrderTable from "./prorderTable";
 
 const { tokens } = themes;
 
-export default function PurchaseRequest() {
+export default function PurchaseOrder() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
-  const [openPROrderModal, setOpenPROrderModal] = useState(false);
-
-  const handlePROrder = () => {
-    setOpenPROrderModal(true);
-  };
-
-  const handleClosePROrder = () => {
-    setOpenPROrderModal(false);
-  };
-
   return (
     <Box sx={{ m: "5px 20px 20px 20px" }}>
-      <PurchaseRequestModal
-        open={openPROrderModal}
-        handleClose={handleClosePROrder}
-        onSuccess={() => {
-          setOpenPROrderModal(false);
-          // handleGetAll();
-        }}
-      />
       {/* HEADER */}
       <Box
         display="flex"
         justifyContent="space-between"
         alignItems="center"
-        sx={{ mb: "40px" }}
+        sx={{ mb: "20px" }}
       >
-        <Header title="Purchase Order" subtitle="Order the items" />
+        <Header title="Purchase Order" subtitle="Purchase order subtitle" />
 
         <Box>
           <Button
@@ -65,38 +45,6 @@ export default function PurchaseRequest() {
           </Button>
         </Box>
       </Box>
-
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "end",
-          position: "absolute",
-          zIndex: 1,
-        }}
-      >
-        <Button
-          onClick={handlePROrder}
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: 200,
-            cursor: "pointer",
-            fontWeight: "bold",
-            backgroundColor: (themeMode) =>
-              themeMode.palette.mode === "dark" ? "#334b5f" : "lightgray",
-            color: colors.grey[100],
-            "&:hover": {
-              textShadow: "0 0 0.5rem rgba(255, 255, 255, 0.75)",
-              color: "#fff",
-              backgroundColor: "gray",
-            },
-          }}
-        >
-          <AddIcon sx={{ mr: 0.5 }} />
-          Purchase Order
-        </Button>
-      </Box>
       <Divider>
         <Typography
           sx={{
@@ -111,6 +59,7 @@ export default function PurchaseRequest() {
       <Box>
         <PurchaseOrderTable />
       </Box>
+      {/* Contents */}
     </Box>
   );
 }
